2f71c18d6b98051168d1e7dcc86ddf0e653feb33
goodsep <goodsep@node-0.shimmy.cuadvnetfall2016-pg0.apt.emulab.net>
2019-05-15 20:30:13 -0600

    subsribe fixed

--- subscribe/subscribe.py
+++ subscribe/subscribe.py
@@ -1,13 +1,16 @@
 import sysv_ipc
-msgSize =16
-shmSize =1024
+import datetime
+msgSize = 16 #100*1024
+shmSize = 1024 #1024*1024*1024*6.4
 
-shm = sysv_ipc.SharedMemory(2006)
+shm = sysv_ipc.SharedMemory(8206)
 shm.attach()
 offset = 0;
 i = 0
 total = 1000
 prev_count = -1
+
+a = datetime.datetime.now()
 while True:
         mem_data = shm.read(msgSize, offset)
         find_data=mem_data.find('\0')
@@ -17,6 +20,9 @@ while True:
                 #print data
                 i += 1
         if i > total:
+                b = datetime.datetime.now()
+                delta = b - a
+                print delta.total_seconds() * 1000 # milliseconds
                 break
         #print offset
         offset = (offset + msgSize) % shmSize
